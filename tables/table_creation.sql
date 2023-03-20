e85310b561c63d58b45f3bc56798a98d9c5de214
Greg Noethlich <gmnoethlich@gmail.com>
2023-03-20 16:24:39 -0500

    tables created

--- tables/table_creation.sql
+++ tables/table_creation.sql
@@ -1,12 +1,18 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
 CREATE TABLE artists (
-    id INT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
     bio TEXT,
     followers BIGINT
 );
 
 CREATE TABLE songs (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(50) NOT NULL,
     release_date DATE,
     length INT NOT NULL,
@@ -14,10 +20,33 @@ CREATE TABLE songs (
 );
 
  CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
     length INT,
     title VARCHAR(50) NOT NULL,
     song_count TINYINT,
     release_date DATE,
     artwork_url VARCHAR(50),
-    release_date DATE
+    release_date DATE NOT NULL
+ );
+
+ CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    length INT NOT NULL
+ );
+
+ CREATE TABLE podcasts (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    host VARCHAR(50) NOT NULL,
+    about VARCHAR(500),
+    category VARCHAR(50)
+ );
+
+ CREATE TABLE podcast_episodes (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(500),
+    length INT NOT NULL,
+    release_date DATE NOT NULL
  );
