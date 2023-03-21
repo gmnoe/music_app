ef619f0b3cf31b74f9a96259dd5e51b6a153b7c0
Greg Noethlich <gmnoethlich@gmail.com>
2023-03-21 14:34:21 -0500

    ER Diagram finished, V Env setup

--- tables/table_creation.sql
+++ tables/table_creation.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(20) NOT NULL UNIQUE,
+    username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
 
@@ -8,7 +8,7 @@ CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL UNIQUE,
     bio TEXT,
-    followers BIGINT
+    followers INT
 );
 
 CREATE TABLE songs (
@@ -16,14 +16,14 @@ CREATE TABLE songs (
     title VARCHAR(50) NOT NULL,
     release_date DATE,
     length INT NOT NULL,
-    plays BIGINT NOT NULL
+    plays INT NOT NULL
 );
 
  CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     length INT,
     title VARCHAR(50) NOT NULL,
-    song_count TINYINT,
+    song_count INT,
     release_date DATE,
     artwork_url VARCHAR(50),
     release_date DATE NOT NULL
@@ -39,14 +39,14 @@ CREATE TABLE songs (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     host VARCHAR(50) NOT NULL,
-    about VARCHAR(500),
+    about TEXT,
     category VARCHAR(50)
  );
 
  CREATE TABLE podcast_episodes (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description VARCHAR(500),
+    description TEXT,
     length INT NOT NULL,
     release_date DATE NOT NULL
  );
