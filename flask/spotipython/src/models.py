3952289888cab0555fee72b87db1509ee1de7e74
Greg Noethlich <gmnoethlich@gmail.com>
2023-04-21 12:25:09 -0500

    table images, models for ORM

--- flask/spotipython/src/models.py
+++ flask/spotipython/src/models.py
@@ -15,35 +15,30 @@ class User(db.Model):
         self.playlists = playlists
         self.followers = followers
         self.following = following
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'username': self.username,
-            'playlists': self.playlists,
-            'followers': self.followers,
-            'following': self.following
-        }
     
 class Artist(db.Model):
     __tablename__ = 'artists'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
-    website = db.Column(db.String(128), unique=True, nullable=True)
-    followers = db.Column(db.Integer)
+    website = db.Column(db.String(128), unique=True)
+    followers = db.Column(db.Integer, nullable=True)
     user_id = db.relationship('User', backref='artist')
 
-    def __inti__(self, name: str, website: str, followers: int, user_id: int)
+    def __init__(self, name: str, website: str, followers: int, user_id: int):
         self.name = name
         self. website = website
         self.followers = followers
         self.user_id = user_id
 
-    def serialize(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'website': self.website,
-            'followers': self.followers,
-            'user_id': self.user_id
-        }
+
+class Song(db.Model):
+    __tablename__= 'songs'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(128), nullable=False)
+    length_mins = db.Column(db.Integer, nullable=False)
+    plays = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, title: str, length_mins: int, plays: int):
+        self.title = title
+        self.length_mins = length_mins
+        self.plays = plays
